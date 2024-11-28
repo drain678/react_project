5f6bb5420ce19d9fcea1fa95fa66dce87ae8e01a
drain678 <ekaterina1.zorina@gmail.com>
2024-11-28 13:31:48 +0300

    fix Menu.jsx

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button } from '@consta/uikit/Button';
 import { Layout } from "@consta/uikit/Layout"
 
 const Menu = () => {
