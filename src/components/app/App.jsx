25f0df329093533bb2867eb9041b812ded54a1b0
drain678 <ekaterina1.zorina@gmail.com>
2024-11-28 13:26:18 +0300

    update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
 
-function App() {
+import { Attachment } from '@consta/uikit/Attachment';
+import { Avatar } from '@consta/uikit/Avatar';
+import { Button } from '@consta/uikit/Button';
+import { IconBackward } from '@consta/icons/IconBackward';
+import { Card } from '@consta/uikit/Card';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ServicePage from '../../pages/service-page/ServicePage';
+import ServiceDetailPage from '../../pages/service-detail-page/ServiceDetailPage';
+import MainPage from '../../pages/main-page/MainPage';
+import MainLayout from '../../layouts/main-layout/MainLayout';
+import { AppRoute } from '../../const';
+
+const App = function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme preset={presetGpnDefault}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.main} element={<MainLayout />}>
+            <Route index element={<MainPage />} />
+            <Route path={AppRoute.service} element={<ServicePage />} />
+            <Route path={`${AppRoute.service}/:id`} element={<ServiceDetailPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Attachment
+        withPictogram
+        fileName="Файл"
+        fileExtension="jpg"
+        fileDescription="14 Мб 01.04.2020, 07:01"
+      />
+      <Card verticalSpace="2xl" horizontalSpace="2xl">
+        <Button label="КНОПИЩА" iconLeft={IconBackward} />
+        <Button label="кнпк поменьше" view="secondary" />
+      </Card>
+    </Theme>
   );
 }
 
-export default App;
+export default App
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Button } from '@consta/uikit/Button';
 
 
 const Menu = () => {
