9b9b4814231c835d6b60dc0d865e6b2ccfd9d45e
drain678 <ekaterina1.zorina@gmail.com>
2024-12-02 13:00:04 +0300

    Update folder  main-page

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -1,64 +1,10 @@
-// import React from "react";
-// import { useState } from "react";
-// import { Card } from '@consta/uikit/Card';
-// import { Button } from '@consta/uikit/Button';
-// import { Text } from '@consta/uikit/Text';
-// import { Layout } from "@consta/uikit/Layout";
-// import { faker } from '@faker-js/faker';
-
-// const MainPage = () => {
-
-//     const [counter, setCounter] = useState({
-//         createdAt: '',
-//         name: '',
-//         description: '',
-//         id: '',
-//     });
-
-//     console.log(counter.incCount)
-
-//     return (
-//         <>
-//             <div>Главная страница</div>
-//             {[1, 2, 3, 4, 5].map((item) => {
-//                 return (<Card
-//                     key={item}
-//                     verticalSpace="xs"
-//                     horizontalSpace="xs">
-//                     <Layout>
-//                         <img src="" alt="" />
-//                         <Layout direction="column">
-//                             <Text>{faker.lorem.sentence()}</Text>
-//                             <Text>{faker.lorem.paragraph()}</Text>
-//                         </Layout>
-//                     </Layout>
-//                 </Card>)
-//             })}
-//             <Button
-//             label='Увеличить значение'
-//                 onClick={() => {
-//                     setCounter((prevState) => ({
-//                         ...prevState,
-//                         count: prevState.count + 1
-//                     }));
-//                 }}
-//             />
-//         </>
-//     )
-// }
-
-// export default MainPage;
-
-
-
-
-import NewsList from "../../components/news-list/NewsList";
+import NewsList from "../../components/news_list/NewsList";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { urlApi } from "../../const";
 
-const MainPage = () => {
-    const URLNews = `${urlApi}main`;
+function MainPage() {
+    const URLNews = urlApi + 'main';
     const [newsState, setNews] = useState([]);
 
     useEffect(() => {
@@ -73,13 +19,10 @@ const MainPage = () => {
     }, [URLNews]);
 
     return (
-        <>
-        <div>Главная страница</div>
         <div>
             <NewsList data={newsState} />
         </div>
-        </>
     );
-};
+}
 
 export default MainPage;
