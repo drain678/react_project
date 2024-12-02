66dd34ca57e5c860613cf90700954675b6b119dd
drain678 <ekaterina1.zorina@gmail.com>
2024-12-02 13:04:21 +0300

    Update folder service-detail-page

--- src/pages/service-detail-page/ServiceDetailPage.jsx
+++ src/pages/service-detail-page/ServiceDetailPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import ServiceDetail from "../../components/service-detail/ServiceDetail";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { urlApi } from "../../const";
 
 const ServiceDetailPage = () => {
+    const serviceId = window.location.pathname.split("/")[2];
+    const URLService = `${urlApi}services/${serviceId}`;
+
+    const [serviceState, setService] = useState({});
+
+    useEffect(() => {
+        const fetchService = async () => {
+            try {
+                const response = await axios.get(URLService);
+                setService(response.data);
+            } catch (error) {
+                console.error("Ошибка загрузки данных услуги:", error);
+            }
+        };
+        fetchService();
+    }, [URLService]);
+
     return (
-        <div>ServiceDetailPage</div>
-    )
-}
+        <ServiceDetail data={serviceState} />
+    );
+};
 
 export default ServiceDetailPage;
