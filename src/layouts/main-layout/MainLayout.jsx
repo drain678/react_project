8bb286bdc15aa04446f23dbbbd43868249206a2a
drain678 <ekaterina1.zorina@gmail.com>
2024-12-03 12:42:27 +0300

    Update footer

--- src/layouts/main-layout/MainLayout.jsx
+++ src/layouts/main-layout/MainLayout.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Outlet } from 'react-router-dom';
-import Header from "../../components/header/header";
+import Header from "../../components/header/header"; 
+import { useSelector } from "react-redux";
+
+import { Button } from "@consta/uikit/Button";
+import { NavLink } from "react-router-dom";
+import AppPage from "../../const";
 
 const MainLayout = () => {
+    const user = useSelector((state) => state.user);
     return (
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Header />
@@ -13,9 +19,34 @@ const MainLayout = () => {
             <footer className="footer">
                 <div className="footer-content">
                     <div className="footer-line"></div>
-                    <span className="footer-text">2024 Моя компания</span>
+                    {!user.isAuthenticated ? (
+                        <>
+                        <div className="header-group left">
+                            <span className="footer-text"></span>
+                            <NavLink to={AppPage.main} className="header-button">
+                                <Button label="Главная страница" />
+                            </NavLink>
+                        </div>
+                        <span className="footer-text">2024 Компания "OZON"</span>
+                        </>
+                        
+                    ) : (
+                        <>
+                        <div className="header-group left">
+                            <span className="footer-text"></span>
+                            <NavLink to={AppPage.main} className="header-button">
+                                <Button label="Главная страница" />
+                            </NavLink>
+                            <NavLink to={AppPage.services} className="header-button">
+                                    <Button label="Услуги компании" />
+                                </NavLink>
+                        </div>
+                                <span className="footer-text">2024 Компания "OZON"</span>
+                        </>
+                    )}
                 </div>
             </footer>
+
         </div>
     );
 };
